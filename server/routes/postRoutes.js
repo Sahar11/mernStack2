231bce78db67edad5f5399514eb70807725bab5d
Sahar <saharhalim151@gmail.com>
2023-11-25 22:41:52 -0500

    create find by id route

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -38,6 +38,17 @@ router.get('/', async (request, response) => {
   }
 });
 
+router.get('/:id', async (request, response) => {
+  try{
+    const { id } = request.params;
+    const post = await Post.findById(id);
+
+    return response.status(200).json({post});
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+})
 router.put('/:id', async (request, response) => {
   try {
     if (
